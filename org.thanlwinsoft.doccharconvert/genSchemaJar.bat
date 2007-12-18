4fe610f01ee5984ab49ab3c5cd6490e3fd9a848e
keith <devnull@localhost>
2007-12-19 03:12:31 +0700

    [svn r1113] set executable

---
+++ org.thanlwinsoft.doccharconvert/genSchemaJar.bat
@@ -0,0 +1,2 @@
+call ..\org.apache.xmlbeans\bin\scomp -debug schemas\DocCharConverter.xsd -out lib\DocCharConvertSchema.jar -src schemas\src
+call ..\org.apache.xmlbeans\bin\scomp -debug schemas\SyllableParser.xsd -out lib\SyllableParserSchema.jar -src schemas\src
